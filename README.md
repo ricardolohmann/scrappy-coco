2c39275589056e9596bab5bf29dbe76675f34193
ricardolohmann <kaduh.ricardo@gmail.com>
2018-09-03 21:46:31 -0300

    Get subreddits

--- README.md
+++ README.md
@@ -3,6 +3,13 @@
 Running
 =======
 
+Start container
 ```shell
 docker-compose up --build
+```
+
+
+Run different commands in container
+```shell
+docker run scrappycoco_app scrapy crawl reddit -o reddit.json -a subreddits=cats;brazil;worldnews
 ```
--- scrappy_coco/scrappy_coco/spiders/reddit.py
+++ scrappy_coco/scrappy_coco/spiders/reddit.py
@@ -7,7 +7,9 @@ from scrappy_coco.items import RedditItem
 class RedditSpider(scrapy.Spider):
     name = 'reddit'
     allowed_domains = []
-    start_urls = ['http://old.reddit.com/']
+
+    def __init__(self, subreddits='', **kwargs):
+        self.start_urls = self.get_start_urls(subreddits)
 
     def parse(self, response):
         for thread in response.css('#siteTable .thing'):
@@ -15,9 +17,17 @@ class RedditSpider(scrapy.Spider):
 
     def parse_thread(self, thread):
         item = RedditItem()
-        item['upvotes'] = thread.css('.score.upvote::text').extract()
-        item['subreddit'] = thread.css('a.subreddit::attr(href)').extract()
-        item['title'] = thread.css('a.title::text').extract()
-        item['thread_url'] = thread.css('a.title::attr(href)').extract()
-        item['comments_url'] = thread.css('a.comments::attr(href)').extract()
+        item['upvotes'] = thread.css('.score.unvoted::attr(title)').extract_first()
+        item['subreddit'] = thread.css('a.subreddit::attr(href)').extract_first()
+        item['title'] = thread.css('a.title::text').extract_first()
+        item['thread_url'] = thread.css('a.title::attr(href)').extract_first()
+        item['comments_url'] = thread.css('a.comments::attr(href)').extract_first()
         return item
+
+    def get_start_urls(self, subreddits):
+        if subreddits:
+            return ['http://old.reddit.com/r/{0}'.format(subreddit)
+                    for subreddit
+                    in subreddits.split(";")]
+        else:
+            return ['http://old.reddit.com/']
